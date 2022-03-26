2ac7866026cd1ffc6f2606f8bbdb45d7ce9662ed
BashchakYevhen <evgeny221@gmail.com>
2022-03-26 23:06:01 +0200

    whith out picture media

--- js/modal.js
+++ js/modal.js
@@ -12,3 +12,19 @@
     refs.modal.classList.toggle("is-hidden");
   }
 })();
+
+
+(() => {
+  const refs = {
+    openMenuBtn: document.querySelector(".menu-open-btn"),
+    closeMenuBtn: document.querySelector(".menu-close-btn"),
+    menu: document.querySelector(".mob-menu"),
+  };
+
+  refs.openMenuBtn.addEventListener("click", toggleMenu);
+  refs.closeMenuBtn.addEventListener("click", toggleMenu);
+
+  function toggleMenu() {
+    refs.menu.classList.toggle("is-hidden");
+  }
+})();
